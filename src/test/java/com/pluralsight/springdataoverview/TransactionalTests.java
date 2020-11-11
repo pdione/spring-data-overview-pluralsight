0bc31f8a3f065d8a2469c5d406e72ea0679fba2a
pdione <dione.pape@gmail.com>
2020-11-11 22:29:57 +0100

    :sparkles: Add @Transactional

---
+++ src/test/java/com/pluralsight/springdataoverview/TransactionalTests.java
@@ -0,0 +1,54 @@
+package com.pluralsight.springdataoverview;
+
+import com.pluralsight.springdataoverview.entity.Flight;
+import com.pluralsight.springdataoverview.repository.FlightRepository;
+import com.pluralsight.springdataoverview.service.FlightsService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionalTests {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    @Autowired
+    private FlightsService flightsService;
+
+    @Before
+    public void setUp() {
+        flightRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldNotRollBackWhenTheresNoTransaction() {
+        try {
+            flightsService.saveFlight(new Flight());
+        } catch (Exception e) {
+            // Do nothing
+        } finally {
+            assertThat(flightRepository.findAll())
+                    .isNotEmpty();
+        }
+    }
+
+    @Test
+    public void shouldRollBackWhenTheresIsTransaction() {
+        try {
+            flightsService.saveFlightTransactional(new Flight());
+        } catch (Exception e) {
+            // Do nothing
+        } finally {
+            assertThat(flightRepository.findAll())
+                    .isEmpty();
+        }
+    }
+
+}
